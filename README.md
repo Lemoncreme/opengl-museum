07680ca21f0ca922fe85d6a66a259566e778e55a
Benjamin Mastripolito <schwalegos@gmail.com>
2018-04-23 09:46:44 -0600

    everythings working except for textures

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# OpenGL
-placeholder for dev branch
+# CSE389 OpenGL Museum Project
+Nyna Dunning & Benjamin Mastripolito
+
+# How To Use
+Spacebar - Cycle through models
+
+Mouse Click - Mouse click and drag to rotate models
+
+Up/Down - Zoom
+
+Scroll Wheel - Zoom
+
+# Museum Model
+[Link to museum model](https://github.com/Lemoncreme/opengl-museum/blob/master/museum_final.blend)
+Textured model:
+![](./media/museum_textured_blender.JPG)
+
+# Screenshots
+![](./media/ss_obj3.png)
+![](./media/ss_obj2.png)
+![](./media/ss_obj1.png)
+![](./media/ss_tri2.png)
+![](./media/ss_tri1.png)
+
+## Museum Plan
+
+### Theme
+Our theme is brutalist architecture / urban art. We will be using a lot of hard edges and sharp angles in our museum design. Here is an [image reference](https://cdn-images-1.medium.com/max/2000/1*S6RfbHdsLvjOzgl0dWhzgA.jpeg).
+
+### Layout
+The museum will include *at least* two rooms. We plan to add more if we feel confident we can accomplish all other vital parts of the museum program.
+We might include a ceiling depending on time constraints.
+The textures we will be using for our museum will be mostly concrete and other rough, stone-like surfaces.
+Our sculpture will resemble a "found items" type artwork. We're not certain about the final shape, but it will be made up of smaller items. We plan to create our own models for this. 
+Finally, our paintings will include urban street art such as graffiti, murals, etc. 
 
--- src/museum.cpp
+++ src/museum.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "shader.h"
 #include "mesh.h"
@@ -15,10 +15,20 @@ using namespace std;
 #define SCR_WIDTH 1280
 #define SCR_HEIGHT 720
 
+void mouseCallback(GLFWwindow *window, double x, double y);
+void scrollCallback(GLFWwindow *window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 
-	int main(int argc, char **argv) {
+double mouseLastX = SCR_WIDTH / 2.0f;
+double mouseLastY = SCR_HEIGHT / 2.0f;
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+//Timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+int main(int argc, char **argv) {
 	//Initialize GLFW
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -27,6 +37,9 @@ void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 	GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "test", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	glfwSetCursorPosCallback(window, mouseCallback);
+	glfwSetScrollCallback(window, scrollCallback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	//Initialize OpenGL context
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -38,20 +51,34 @@ void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 	glEnable(GL_DEPTH_TEST);
 
 	//Load everything
-	Model model("resource/museum.obj");
+	Model model("model/museum_final.blend");
 	puts("Model created!");
-	Camera camera(0, 0, 0, 0, 0, 0, 0, 0);
-	puts("Camera created!");
-	Shader shader("src/shader.vert", "src/shader.frag");
+	Shader shader("src/shader.vs", "src/shader.fs");
 	puts("Shader created!");
 
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		processInput(window);
 
 		glClearColor(0.2, 0.3, 0.3, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shader.use();
+
+		//Camera projection
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), 
+				(float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		shader.setMat4("projection", projection);
+		shader.setMat4("view", view);
+
+		glm::mat4 modelMatrix;
+		modelMatrix = glm::translate(modelMatrix, glm::vec3(0.0, -1.75, 0.0));
+		modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2, 0.2, 0.2));
+		shader.setMat4("model", modelMatrix);
 		model.Draw(shader);
 
 		glfwSwapBuffers(window);
@@ -62,12 +89,31 @@ void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 }
 
 void processInput(GLFWwindow *window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
-	}
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
+void mouseCallback(GLFWwindow *window, double x, double y) {
+	float xoffset = x - mouseLastX;
+	float yoffset = mouseLastY - y;
+	mouseLastX = x;
+	mouseLastY = y;
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scrollCallback(GLFWwindow *window, double xoffset, double yoffset) {
+	camera.ProcessMouseScroll(yoffset);
+}
